4edd574e53d3b3fc4006132d2eaa4653ebd3545d
tobiassilfverberg <tobiassilfverberg@gmail.com>
2021-11-05 15:44:48 +0100

    tjena

--- dag-9/28-workshop-lightbox/app.js
+++ dag-9/28-workshop-lightbox/app.js
@@ -1,13 +1,46 @@
 /**
- * 28 - Workshop: Lightbox
+ * Workshop: Lightbox
+ *
+ * Två möjliga tillvägagångssätt
+ *
+ * 1a. Hämta ut alla `a.photo`, loopa över dem och lägg till en
+ * click-EventListener för varje element.
+ *
+ * 1b. Hämta ut `.photos`-elementet och lägg till en click-EventListener
+ * på den. I EventListener:n, kolla om det som klickades på är
+ * en IMG.
+ *
+ * 2. Hämta ut IMG's förälder (A-tagg) href och sätt på `#lightbox-wrapper img` src-attribut.
+ *
+ * 3. Lägg till klassen `show` på `#lightbox-wrapper`.
+ *
+ * 4. (Lägg till en click-EventListener på `#lightbox-wrapper` som tar bort `show`-klassen.)
+ *
+ * 5. 🌟 Om du kan fixa så man kan ha en caption på varje bild och den visas. Naturligtvis
+ * via JavaScript, lägg inte till en caption i själva bildfilen :P.
  *
  */
 
-const imagesEl = document.querySelectorAll("a");
+const imagesPhotoEl = document.querySelectorAll("a.photo");
+const imagesPhotoImageEl = document.querySelector("#lightbox-img");
 const lightboxWrapperEl = document.querySelector("#lightbox-wrapper");
+const imgCaption = document.querySelector(".caption");
 
-imagesEl.addEventListener("click", (e) => {
-  e.preventDefault();
+imagesPhotoEl.forEach((image) =>
+  image.addEventListener("click", (e) => {
+    //prevent img from opening in new window
+    e.preventDefault();
+    // steg 2
+    let photoParent = e.target.parentElement.getAttribute("href");
+    imagesPhotoImageEl.setAttribute("src", photoParent);
+    // steg 3
+    lightboxWrapperEl.classList.add("show");
+    // steg 5
+    imgCaption.innerHTML = photoParent;
+  })
+);
 
-  lightboxWrapperEl.classList.add("show");
+// steg 4
+lightboxWrapperEl.addEventListener("click", () => {
+  lightboxWrapperEl.classList.remove("show");
 });
