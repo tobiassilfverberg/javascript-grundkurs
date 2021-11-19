935be9ceaac99e67c13de74e14647d353ea01f25
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2021-11-19 10:00:19 +0100

    Intro to promises

--- dag-15/35-promises/script.js
+++ dag-15/35-promises/script.js
@@ -3,3 +3,28 @@
  *
  */
 
+const teacherIsNice = false;
+
+const willWeGetTheAnswersToTheExam = () => {
+  return new Promise((resolve, reject) => {
+    // do something that takes some time to do
+    // and when done, resolve or reject promise
+    setTimeout(() => {
+      if (teacherIsNice) {
+        // resolve promise
+        resolve();
+      } else {
+        // reject promise
+        reject();
+      }
+    }, 5000);
+  });
+};
+
+willWeGetTheAnswersToTheExam()
+  .then(() => {
+    alert("Promise has been fulfilled. YAY!");
+  })
+  .catch(() => {
+    alert("You always make promises but never hold them.");
+  });
