6cd381ed36bc6b5e047a98a025db5d15d4117ad0
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2021-11-19 13:55:22 +0100

    Proper fetch with "throw new Error"

--- dag-15/36-fetch/script.js
+++ dag-15/36-fetch/script.js
@@ -1,5 +1,31 @@
 /**
  * Fetch
- * 
+ *
  */
 
+/* 
+fetch("data/ogs.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("got data!:", data);
+  })
+  .catch((err) => {
+    console.log("Dogs rejected:", err);
+  });
+ */
+
+fetch("data/horses.json")
+  .then((res) => {
+    // Trigger an error
+    if (!res.ok) {
+      throw new Error("Response was not ok");
+    }
+
+    return res.json();
+  })
+  .then((data) => {
+    console.log("got data!:", data);
+  })
+  .catch((err) => {
+    console.log("Dogs rejected:", err);
+  });
