74a918e82cd6d0701ef10bc0bf25768f06932e56
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-29 15:48:11 +0200

    Weather app workshop. Handle city not found, show loading spinner and show icon matching weather.

--- dag-18/01-weather-app/assets/js/app.js
+++ dag-18/01-weather-app/assets/js/app.js
@@ -3,3 +3,58 @@
  *
  */
 
+const renderCurrentWeather = async (data) => {
+	const icons = data.weather.map((icon) => icon.icon)
+
+	if (!data) {
+		document.querySelector(
+			"#forecast"
+		).innerHTML = `<h3> The city you requested does not exist. Try again</h3>`
+	} else {
+		document.querySelector("#forecast").innerHTML = `
+	<img src="assets/images/forecast-banner.png" class="card-img-top"/>
+		<div class="card-body">
+			<ul class="city card-title conditions" id="location">
+				<li id="city">
+					<h5>
+						<span id="city"> ${data.name}, </span> 
+						<span id="country"> ${data.sys.country} </span> 
+					</h5>
+				</li>
+				<li id="icon">
+					<img src="http://openweathermap.org/img/wn/${icons}@2x.png"/> 
+				</li>
+				<li id="temperature" class="temp">${data.main.temp} &deg;C</li>
+				<li id="humidity" class="humidity">${data.main.humidity}&percnt;     humidity</li>
+				<li class="wind" id="windspeed">${data.wind.speed} m/s</li>
+			</ul>
+		</div>
+	`
+	}
+}
+
+document.querySelector("#search-form").addEventListener("submit", async (e) => {
+	e.preventDefault()
+
+	document.querySelector("#loadingspinner").classList.remove("hide")
+
+	const query = document.querySelector("#query").value
+
+	if (query.length < 3) {
+		alert("Plese enter at least 3 chars")
+		return
+	}
+
+	console.log("Searching for city:", query)
+
+	const data = await getCurrentWeather(query)
+
+	setTimeout(() => {
+		renderCurrentWeather(data)
+		// document.querySelector("#forecast").classList.add("hide")
+		// document.querySelector("#loadingspinner").classList.remove("hide")
+	}, 500) 
+
+	document.querySelector("#forecast").classList.remove("hide")
+	document.querySelector("#loadingspinner").classList.add("hide")
+})
---
+++ dag-18/01-weather-app/assets/js/owmapi.js
@@ -0,0 +1,22 @@
+/**
+ * Open Weather Map API
+ */
+
+const API_KEY = "0ade3d8302cf07178fe9ac867843cd14"
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+const getCurrentWeather = async (query) => {
+	// get weather for query from OpenWeatherMap API
+	const response = await fetch(
+		`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`
+	)
+
+	// convert response from JSON
+	const data = await response.json()
+
+	if (data.cod == 404) {
+		return false
+	} else {
+		return data
+	}
+}
