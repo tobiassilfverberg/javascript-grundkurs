590c7616a8221f44d753e1c335295068729940d5
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-30 10:35:52 +0200

    Increase and decrease salary with methods

--- dag-19/02-vue-basics/app.js
+++ dag-19/02-vue-basics/app.js
@@ -12,6 +12,18 @@ const app = Vue.createApp({
 			msg: "Hello, Vue!",
 		}
 	},
+	methods: {
+		increaseSalary(amount = 1) {
+			this.salary += amount
+		},
+		decreaseSalary(amount = 1) {
+			if (this.salary - amount < 5) {
+				this.salary = 5
+				return
+			}
+			this.salary -= amount
+		},
+	},
 })
 
 // Mount our Vue app to the element with id #app in the DOM
