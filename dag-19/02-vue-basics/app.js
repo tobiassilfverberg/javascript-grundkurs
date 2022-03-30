e0631960116ef82e8d9c9842f808a337d2ef93a8
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-30 09:49:30 +0200

    Click me button increase by 1. Salary buttons function added to increse or decrease salary.

--- dag-19/02-vue-basics/app.js
+++ dag-19/02-vue-basics/app.js
@@ -2,3 +2,17 @@
  * Vue.js Basics
  *
  */
+
+// Create our Vue app
+const app = Vue.createApp({
+	data() {
+		return {
+			count: 0,
+			salary: 10,
+			msg: "Hello, Vue!",
+		}
+	},
+})
+
+// Mount our Vue app to the element with id #app in the DOM
+app.mount("#app")
