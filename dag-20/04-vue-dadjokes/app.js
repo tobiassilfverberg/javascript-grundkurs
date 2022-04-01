561d23fb9b141103415146a76137a183a3b4650b
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-01 14:21:32 +0200

    Fetch with Vue

--- dag-20/04-vue-dadjokes/app.js
+++ dag-20/04-vue-dadjokes/app.js
@@ -7,15 +7,50 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			error: false,
+			joke: null,
+			loading: true,
 		}
 	},
 
-	methods: {
+	mounted() {
+		this.getDadJoke()
 	},
 
-	computed: {
-	}
+	methods: {
+		async getDadJoke() {
+			// set joke to null and loading to true
+			this.joke = null
+			this.loading = true
+
+			// fetch joke from icanhazdadjoke.com
+			try {
+				const res = await fetch("https://icanhazdadjoke.com", {
+					headers: {
+						Accept: "application/json",
+					},
+				})
+
+				// parse response as json
+				const data = await res.json()
+
+				// set loading to false
+				this.loading = false
+
+				// set this.joke to be the response's joke-property
+				this.joke = data.joke
+			} catch (e) {
+				// set loading to false
+				this.loading = false
+
+				// set error to true
+				this.error = true
+			}
+		},
+	},
+
+	computed: {},
 })
 
 // Mount our Vue app on the element with id `app` in the DOM
-app.mount('#app')
+app.mount("#app")
