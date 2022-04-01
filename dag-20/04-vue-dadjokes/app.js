436ed5d0497c30fadc9f9182ff5c906184025b14
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-01 13:05:06 +0200

    Vue dadjokes boilerplate

---
+++ dag-20/04-vue-dadjokes/app.js
@@ -0,0 +1,21 @@
+/**
+ * Vue.js Dad Jokes
+ *
+ */
+
+// Create our Vue app
+const app = Vue.createApp({
+	data() {
+		return {
+		}
+	},
+
+	methods: {
+	},
+
+	computed: {
+	}
+})
+
+// Mount our Vue app on the element with id `app` in the DOM
+app.mount('#app')
