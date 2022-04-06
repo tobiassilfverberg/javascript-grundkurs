a955bbd879f620500be590924dfff4924650d265
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-06 11:03:35 +0200

    Added functionality to show/hide salary section

--- dag-21/src/App.js
+++ dag-21/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
 	])
 
 	const [salary, setSalary] = useState(10)
+	const [showSalary, setShowSalary] = useState(true)
 
 	const addLike = (post) => {
 		post.likes++
@@ -61,48 +62,65 @@ const App = () => {
 
 			<hr />
 
-			<p>Salary per hour: {salary} &euro;</p>
+			<button
+				className="btn btn-primary"
+				onClick={() => setShowSalary(!showSalary)}
+			>
+				Show
+			</button>
 
-			<div className="buttons">
-				<div className="mb-1">
-					<button
-						className="btn btn-primary btn-lg"
-						onClick={() => {
-							changeSalary(1)
-						}}
-					>
-						Raise 1 &euro; 🤑
-					</button>
+			{showSalary && (
+				<div>
+					<p>Salary per hour: {salary} &euro;</p>
 
-					<button
-						className="btn btn-warning btn-lg"
-						onClick={() => {
-							changeSalary(-1)
-						}}
-					>
-						Decrease 1 &euro; 😢
-					</button>
+					{salary < 12 && (
+						<div className="alert alert-warning">
+							You might want to get a second job?
+						</div>
+					)}
+
+					<div className="buttons">
+						<div className="mb-1">
+							<button
+								className="btn btn-primary btn-lg"
+								onClick={() => {
+									changeSalary(1)
+								}}
+							>
+								Raise 1 &euro; 🤑
+							</button>
+
+							<button
+								className="btn btn-warning btn-lg"
+								onClick={() => {
+									changeSalary(-1)
+								}}
+							>
+								Decrease 1 &euro; 😢
+							</button>
+						</div>
+
+						<div className="mb-1">
+							<button
+								className="btn btn-success btn-lg"
+								onClick={() => {
+									changeSalary(5)
+								}}
+							>
+								Raise 5 &euro; 🤑🤑🤑
+							</button>
+							<button
+								className="btn btn-danger btn-lg"
+								onClick={() => {
+									changeSalary(-5)
+								}}
+							>
+								Decrease 5 &euro; 😢😢😢
+							</button>
+						</div>
+					</div>
 				</div>
-
-				<div className="mb-1">
-					<button
-						className="btn btn-success btn-lg"
-						onClick={() => {
-							changeSalary(5)
-						}}
-					>
-						Raise 5 &euro; 🤑🤑🤑
-					</button>
-					<button
-						className="btn btn-danger btn-lg"
-						onClick={() => {
-							changeSalary(-5)
-						}}
-					>
-						Decrease 5 &euro; 😢😢😢
-					</button>
-				</div>
-			</div>
+			)}
 
 			<hr />
 
