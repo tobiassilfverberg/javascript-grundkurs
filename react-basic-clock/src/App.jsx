7e02a2c43b4a3d3b782a6aa67e6ec4b6d2aaf55f
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-08 11:42:52 +0200

    Clock skeleton

---
+++ react-basic-clock/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+const App = () => {
+
+	return (
+		<div className="container">
+			<div className="display-1 text-center">
+				00:13:37
+			</div>
+		</div>
+	)
+}
+
+export default App;
