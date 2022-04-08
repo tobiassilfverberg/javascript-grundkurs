ed450be7589dbbcf2aa869c8e07cbceb0e3ceaca
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-08 11:56:09 +0200

    Basic clock

--- react-basic-clock/src/App.jsx
+++ react-basic-clock/src/App.jsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from 'react'
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
 
 const App = () => {
+	const [time, setTime] = useState("00:13:37")
+
+	// This will be executed only once
+	useEffect(() => {
+		setInterval(() => {
+			const now = new Date().toLocaleTimeString()
+			setTime(now)
+		}, 1000)
+	}, [])
 
 	return (
 		<div className="container">
-			<div className="display-1 text-center">
-				00:13:37
-			</div>
+			<div className="display-1 text-center">{time}</div>
 		</div>
 	)
 }
 
-export default App;
+export default App
