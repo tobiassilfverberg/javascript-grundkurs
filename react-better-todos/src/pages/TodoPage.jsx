62aa2cb700b27426a0279984e15cfcae9b0e0597
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-19 13:39:17 +0200

    Get a single todo and display in the browser.

--- react-better-todos/src/pages/TodoPage.jsx
+++ react-better-todos/src/pages/TodoPage.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
+import Button from "react-bootstrap/Button"
 import { useParams } from "react-router-dom"
+import TodosAPI from "../services/TodosAPI"
 
 const TodoPage = () => {
+	const [todo, setTodo] = useState()
 	const { id } = useParams()
 
-	return <div>Show todo with id {id}.</div>
+	useEffect(() => {
+		const getTodo = async () => {
+			const data = await TodosAPI.getTodo(id)
+			setTodo(data)
+		}
+		getTodo()
+	}, [id])
+
+	if (!todo) {
+		return <p>Loading....</p>
+	}
+
+	return (
+		<div>
+			<h3> {todo.title} </h3>
+
+			<p>
+				<strong>Status:</strong>{" "}
+				{todo.completed ? "Completed" : "Not completed"}{" "}
+			</p>
+
+			<Button variant="success"> Toggle </Button>
+			<Button variant="warning"> Edit </Button>
+		</div>
+	)
 }
 
 export default TodoPage
