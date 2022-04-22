6ac23028e394ce05c767bfb83bbdccd5d4dd40b9
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-22 10:29:58 +0200

    Implemented get request in the service

--- hackernews/src/services/HackerNewsAPI.js
+++ hackernews/src/services/HackerNewsAPI.js
@@ -3,3 +3,16 @@
  *
  * <https://hn.algolia.com/api>
  */
+
+import axios from "axios"
+
+axios.defaults.baseURL = "https://hn.algolia.com/api/v1"
+
+const get = async (endpoint) => {
+	const response = await axios.get(endpoint)
+	return response.data
+}
+
+export const search = async (query, page) => {
+	return get(`/search?query=${query}&tags=story&page=${page}`)
+}
