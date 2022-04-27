8b7b53200c7c22da817712a7492e6777724af89d
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-27 11:31:10 +0200

    Moved functionallity of changing theme to nav bar so you can access in anywhere on the page.

--- hackernews/src/components/Navigation.jsx
+++ hackernews/src/components/Navigation.jsx
@@ -1,18 +1,39 @@
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { Link, NavLink } from 'react-router-dom'
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import { Link, NavLink } from "react-router-dom"
+
+import { ThemeContext } from "../contexts/ThemeContextProvider"
+import { useContext } from "react"
 
 const Navigation = () => {
+	const { isDarkTheme, theme, toggleTheme } = useContext(ThemeContext)
+
 	return (
-		<Navbar bg="dark" variant="dark" expand="md" className="border-bottom border-light">
+		<Navbar
+			bg="dark"
+			variant="dark"
+			expand="md"
+			className="border-bottom border-light"
+		>
 			<Container>
-				<Navbar.Brand as={Link} to="/">Hacker News</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">
+					Hacker News
+				</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/search">Search</Nav.Link>
+						<Nav.Link as={NavLink} end to="/search">
+							Search
+						</Nav.Link>
+						<Button
+							onClick={toggleTheme}
+							variant="secondary"
+						>
+							{isDarkTheme() ? "Lightmode" : "Darkmode"}
+						</Button>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
--- hackernews/src/pages/HomePage.jsx
+++ hackernews/src/pages/HomePage.jsx
@@ -1,25 +1,14 @@
-import { useContext } from "react"
 import Button from "react-bootstrap/Button"
 import { Link } from "react-router-dom"
-import { ThemeContext } from "../contexts/ThemeContextProvider"
 
 const HomePage = () => {
-	const { isDarkTheme, theme, toggleTheme } = useContext(ThemeContext)
-
+	
 	return (
 		<>
 			<h1>Welcome to Hacker News!</h1>
 
 			<Button variant="primary" as={Link} to="/search">
 				Use the Search, you must
-			</Button>
-
-			<p>Your theme is: {theme}</p>
-
-			<Button onClick={toggleTheme} variant="success">
-				{isDarkTheme()
-					? "Too dark, light plz!"
-					: "Way too bright, can't see"}
 			</Button>
 		</>
 	)
