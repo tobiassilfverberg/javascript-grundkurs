db2ad68b1c58b7a1ffe679ae6251ca2bbf4d77f2
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-29 09:42:09 +0200

    Workshop solution.

---
+++ hackernews/src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, useCallback } from "react"
+import axios from "axios"
+
+const useFetch = (initialUrl = null) => {
+	const [url, setUrl] = useState(initialUrl)
+	const [data, setData] = useState()
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
+
+	const fetchData = useCallback(async () => {
+		setLoading(true)
+		try {
+			const res = await axios.get(url)
+			await new Promise((r) => setTimeout(r, 1000))
+			setData(res.data)
+		} catch (err) {
+			setError(err)
+		}
+		setLoading(false)
+	}, [url])
+
+	useEffect(() => {
+		if (!url) {
+			return
+		}
+		setLoading(true)
+
+		fetchData()
+		setLoading(false)
+	}, [fetchData, url])
+
+	return {
+		data,
+		error,
+		loading,
+		fetchData,
+	}
+}
+
+export default useFetch
---
+++ hackernews/src/pages/FetchPage.jsx
@@ -0,0 +1,27 @@
+import useFetch from "../hooks/useFetch"
+import Button from "react-bootstrap/Button"
+
+const FetchPage = () => {
+	const baseURL = "https://api.chucknorris.io/jokes/random"
+
+	const { data, error, loading, fetchData } = useFetch(baseURL)
+
+	return (
+		<>
+			<h1>Fetch Page</h1>
+			{loading && <p>Loading...</p>}
+
+			{error && <p>Error: {error.message}</p>}
+
+			{!loading && (
+				<>{data && <p className="display-1">{data.value}</p>}</>
+			)}
+
+			<Button disabled={loading} onClick={fetchData}>
+				Get new joke
+			</Button>
+		</>
+	)
+}
+
+export default FetchPage
