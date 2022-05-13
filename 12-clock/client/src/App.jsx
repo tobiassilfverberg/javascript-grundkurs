5d5f866273272f07936fdee78236cab172891a5d
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-05-13 10:21:43 +0200

    Connect the client to the server and changed debug from 'chat' to 'clock'.

--- 12-clock/client/src/App.jsx
+++ 12-clock/client/src/App.jsx
@@ -1,9 +1,11 @@
 import socketio from "socket.io-client"
-import Timer from './components/Timer';
-import './App.css';
+import Timer from "./components/Timer"
+import "./App.css"
 
 const App = () => {
 	const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
+
+	socket.emit("hewwo")
 
 	return (
 		<div className="container text-center">
@@ -12,4 +14,4 @@ const App = () => {
 	)
 }
 
-export default App;
+export default App
--- 12-clock/server/controllers/socket_controller.js
+++ 12-clock/server/controllers/socket_controller.js
@@ -2,27 +2,31 @@
  * Socket Controller
  */
 
-const debug = require('debug')('chat:socket_controller');
+const debug = require("debug")("clock:socket_controller");
 let io = null; // socket.io server instance
 
 /**
  * Handle a user disconnecting
  *
  */
-const handleDisconnect = function() {
+const handleDisconnect = function () {
 	debug(`Client ${this.id} disconnected :(`);
-}
+};
 
 /**
  * Export controller and attach handlers to events
  *
  */
-module.exports = function(socket, _io) {
+module.exports = function (socket, _io) {
 	// save a reference to the socket.io server instance
 	io = _io;
 
-	debug(`Client ${socket.id} connected`)
+	debug(`Client ${socket.id} connected`);
 
 	// handle user disconnect
-	socket.on('disconnect', handleDisconnect);
-}
+	socket.on("disconnect", handleDisconnect);
+};
+
+socket.on("hewwo", function () {
+	debug(`Client ${socket.id} said hello 😍`);
+});
